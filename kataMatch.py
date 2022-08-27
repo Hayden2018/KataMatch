fc20700662c2bfd9a1a9b3106c96f8e10ffccdda
Hayden2018 <39459225+Hayden2018@users.noreply.github.com>
2022-08-27 15:11:45 +0800

    Add support for handicap

--- kataMatch.py
+++ kataMatch.py
@@ -1,13 +1,14 @@
 import subprocess
 from subprocess import PIPE, DEVNULL
 import json
-from time import sleep, time
+from time import time
+from os import path
 
 default_cfg = {
   'size': 19,
-  'komi': 6.5,
+  'komi': 7.5,
   'handicap': 0,
-  'rules': 'tromp-taylor'
+  'rules': 'chinese'
 }
 
 def __create_query(visit, initial, moves, game_cfg):
@@ -28,12 +29,40 @@ def __get_data(out):
   m = data['moveInfos'][0]['move']
   return m, data
 
+def __get_initial(handicap):
+  stones = []
+  if handicap >= 2:
+    stones.append(['B', 'Q16'])
+    stones.append(['B', 'D4'])
+  if handicap >= 3:
+    stones.append(['B', 'D16'])
+  if handicap >= 4:
+    stones.append(['B', 'D16'])
+  if handicap >= 5:
+    stones.append(['B', 'K10'])
+  if handicap >= 6:
+    stones.pop()
+    stones.append(['B', 'D10'])
+    stones.append(['B', 'Q10'])
+  if handicap >= 7:
+    stones.append(['B', 'Q10'])
+  if handicap >= 8:
+    stones.pop()
+    stones.append(['B', 'K16'])
+    stones.append(['B', 'K4'])
+  if handicap >= 8:
+    stones.append(['B', 'K10'])
+  return stones
 
-def match(engine_path, black_cfg, white_cfg, n, game_cfg=default_cfg, logging=True, switch=True):
-  black_engine = subprocess.Popen([engine_path, 'analysis', '-config', 'analysis_example.cfg',  '-model', black_cfg['weight']], stdin=PIPE, stdout=PIPE, stderr=DEVNULL)
-  white_engine = subprocess.Popen([engine_path, 'analysis', '-config', 'analysis_example.cfg',  '-model', white_cfg['weight']], stdin=PIPE, stdout=PIPE, stderr=DEVNULL)
 
-  sleep(3)
+def match(engine_path, black_cfg, white_cfg, config_path=None, n=10, game_cfg=default_cfg, logging=True, switch=True):
+
+  if config_path is None:
+    config_path = path.join(path.dirname(engine_path), 'analysis_example.cfg')
+
+  black_engine = subprocess.Popen([engine_path, 'analysis', '-config', config_path, '-model', black_cfg['weight']], stdin=PIPE, stdout=PIPE, stderr=DEVNULL)
+  white_engine = subprocess.Popen([engine_path, 'analysis', '-config', config_path, '-model', white_cfg['weight']], stdin=PIPE, stdout=PIPE, stderr=DEVNULL)
+
   record = []
   result = {
     'black': 0,
@@ -41,7 +70,8 @@ def match(engine_path, black_cfg, white_cfg, n, game_cfg=default_cfg, logging=Tr
   }
 
   k = 0
-  if switch and game_cfg['komi'] > 0:
+  with_handicap = game_cfg['handicap'] >= 2
+  if switch and not with_handicap:
     k = n // 2
     n = n - k
 
@@ -51,24 +81,42 @@ def match(engine_path, black_cfg, white_cfg, n, game_cfg=default_cfg, logging=Tr
     white_pass = False
     data = None
 
-    initial = []
+    initial = __get_initial(game_cfg['handicap'])
     moves = []
 
     while (not black_pass) or (not white_pass):
 
-      black_engine.stdin.write(__create_query(black_cfg['visit'], initial, moves, game_cfg).encode())
-      black_engine.stdin.flush()
-      out = black_engine.stdout.readline()
-      m, data = __get_data(out)
-      moves.append(['b', m])
-      black_pass = True if m == 'pass' else False
+      if with_handicap:
 
-      white_engine.stdin.write(__create_query(white_cfg['visit'], initial, moves, game_cfg).encode())
-      white_engine.stdin.flush()
-      out = white_engine.stdout.readline()
-      m, data = __get_data(out)
-      moves.append(['w', m])
-      white_pass = True if m == 'pass' else False
+        white_engine.stdin.write(__create_query(white_cfg['visit'], initial, moves, game_cfg).encode())
+        white_engine.stdin.flush()
+        out = white_engine.stdout.readline()
+        m, data = __get_data(out)
+        moves.append(['W', m])
+        white_pass = True if m == 'pass' else False
+
+        black_engine.stdin.write(__create_query(black_cfg['visit'], initial, moves, game_cfg).encode())
+        black_engine.stdin.flush()
+        out = black_engine.stdout.readline()
+        m, data = __get_data(out)
+        moves.append(['B', m])
+        black_pass = True if m == 'pass' else False
+
+      else:
+
+        black_engine.stdin.write(__create_query(black_cfg['visit'], initial, moves, game_cfg).encode())
+        black_engine.stdin.flush()
+        out = black_engine.stdout.readline()
+        m, data = __get_data(out)
+        moves.append(['B', m])
+        black_pass = True if m == 'pass' else False
+
+        white_engine.stdin.write(__create_query(white_cfg['visit'], initial, moves, game_cfg).encode())
+        white_engine.stdin.flush()
+        out = white_engine.stdout.readline()
+        m, data = __get_data(out)
+        moves.append(['W', m])
+        white_pass = True if m == 'pass' else False
 
     if data['moveInfos'][0]['winrate'] > 0.5:
       result['black'] += 1
@@ -82,10 +130,6 @@ def match(engine_path, black_cfg, white_cfg, n, game_cfg=default_cfg, logging=Tr
       if logging:
         print('Match', j + 1)
         print('White Engine Win')
-
-  if not (switch and game_cfg['komi'] > 0):
-    result['record'] = record
-    return result
 
   # Play with black and white reversed
   for j in range(k):
@@ -103,7 +147,7 @@ def match(engine_path, black_cfg, white_cfg, n, game_cfg=default_cfg, logging=Tr
       white_engine.stdin.flush()
       out = white_engine.stdout.readline()
       m, data = __get_data(out)
-      moves.append(['b', m])
+      moves.append(['B', m])
       white_pass = True if m == 'pass' else False
 
       black_engine.stdin.write(__create_query(black_cfg['visit'], initial, moves, game_cfg).encode())
@@ -133,16 +177,16 @@ def match(engine_path, black_cfg, white_cfg, n, game_cfg=default_cfg, logging=Tr
 if __name__ == '__main__':
 
   black_cfg = {
-    'weight': 'katago-v1.10.0\kata1-b10c128-11500.txt.gz',
-    'visit': 24
+    'weight': '\katago-v1.10.0\kata1-b15c192-12200.txt.gz',
+    'visit': 18
   }
 
   white_cfg = {
-    'weight': 'katago-v1.10.0\kata1-b10c128-11500.txt.gz',
+    'weight': '\katago-v1.10.0\kata1-b15c192-12200.txt.gz',
     'visit': 12
   }
 
   engine_path = 'katago-v1.10.0\katago.exe'
 
-  result = match(engine_path, black_cfg, white_cfg, 10, switch=True)
+  result = match(engine_path, black_cfg, white_cfg, n=20, switch=True)
   print(result)
